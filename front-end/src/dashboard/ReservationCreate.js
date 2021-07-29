177d50571f873d4bd09c0de4c86377f2346e3ee4
erichill41 <ehill4182@yahoo.com>
2021-07-28 20:32:56 -0600

    Continued to edit Create Reservation form

--- front-end/src/dashboard/ReservationCreate.js
+++ front-end/src/dashboard/ReservationCreate.js
@@ -15,6 +15,9 @@ function ReservationCreate() {
     people: "1",
   })
 
+  // TODO Create Change Handler
+  // TODO Create Submit Handler
+
   return (
     <main>
       <h1> Create a Reservation </h1>
@@ -31,6 +34,7 @@ function ReservationCreate() {
               name="first_name"
               type="text"
               required="true"
+              value={reservation.first_name}
             />
             <small className="form-text text-muted"> Enter First Name </small>
           </div>
@@ -44,8 +48,50 @@ function ReservationCreate() {
               name="last_name"
               type="text"
               required="true"
+              value={reservation.last_name}
             />
             <small className="form-text text-muted"> Enter Last Name </small>
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-4 form-group">
+            <label className="form-label" htmlFor="mobile_number">
+              Mobile Number
+            </label>
+            <input
+              className="form-control"
+              id="mobile_number"
+              name="mobile_number"
+              type="text"
+              required="true"
+              placeholder="(xxx) xxx-xxxx"
+              value={reservation.mobile_number}
+            />
+            <small className="form-text text-muted"> Enter Mobile Number </small>
+          </div>
+          <div className="col-4 form-group">
+            <label className="form-label" htmlFor="mobile_number">
+              Party Size
+            </label>
+            <select 
+              className="form-control"
+              id="people"
+              name="people"
+              required={true}
+            >
+              <option> 1 </option>
+              <option> 2 </option>
+              <option> 3 </option>
+              <option> 4 </option>
+              <option> 5 </option>
+              <option> 6 </option>
+              <option> 7 </option>
+              <option> 8 </option>
+              <option> 9 </option>
+              <option> 10+ </option>
+
+            </select>
+            <small className="form-text text-muted"> Select Party Size </small>
           </div>
         </div>
       </form>
